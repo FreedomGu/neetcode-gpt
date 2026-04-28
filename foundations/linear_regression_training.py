4a4527c126c583cbbfbf4d678b0f2328cda21725
Yuming Gu <42958381+FreedomGu@users.noreply.github.com>
2026-04-28 00:33:15 -0500

    Implement Linear regression training (Math Foundations 6/6)

---
+++ foundations/linear_regression_training.py
@@ -0,0 +1,17 @@
+import numpy as np
+from numpy.typing import NDArray
+
+class Solution:
+
+    def get_model_prediction(self, X: NDArray[np.float64], weights: NDArray[np.float64]) -> NDArray[np.float64]:
+        # X is (n, m), weights is (m,) -> return (n,) predictions
+        # Round to 5 decimal places
+        out = X @ weights 
+        return np.round(out, 5)
+
+    def get_error(self, model_prediction: NDArray[np.float64], ground_truth: NDArray[np.float64]) -> float:
+        # Compute mean squared error between predictions and ground truth
+        # Round to 5 decimal places
+        a = 1/ model_prediction.shape[0]
+        out = a * np.sum((model_prediction - ground_truth)**2)
+        return round(out, 5)
